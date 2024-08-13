17b42f0dddbabd69a3d637abb91c1d486858691b
AAgboola <A_Agboola18@cranford.hounslow.sch.uk>
2024-08-13 14:02:48 +0100

    added stars

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 from os.path import join
+from random import randint
+
+
 # General setup
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
@@ -11,8 +14,10 @@ running = True
 surf = pygame.Surface((100, 200))
 x = 100
 
-
+stars = pygame.image.load(join('images', 'star.png')).convert_alpha()
 player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
+star_position = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT))for i in range(20)]
+
 while running:
     # Event loop
     for event in pygame.event.get():
@@ -22,6 +27,8 @@ while running:
     # Fill the display with blue color
     display_surface.fill('dark gray')
     x += 0.1
+    for pos in star_position:
+        display_surface.blit(stars, pos)
     display_surface.blit(player_surf, (x, 150))
     pygame.display.update()
 
